778c95e2742cd2ae3f0106dd3fe17485c9a5c374
arminsadeghian <eng.a.sadeghian@gmail.com>
2022-10-27 18:08:02 +0330

    Git Init

---
+++ bootstrap/config.php
@@ -0,0 +1,13 @@
+<?php
+
+use Kavenegar\KavenegarApi;
+
+$database_config = (object)[
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => '',
+    'dbname' => '7auth',
+    'charset' => 'utf8mb4'
+];
+
+$api = new KavenegarApi('6A39424E305366312F57384D71544F674C756D486C7556566C665A6B73746332577050477132715071396B3D');
---
+++ bootstrap/constants.php
@@ -0,0 +1,5 @@
+<?php
+
+const SITE_TITLE = '7Auth Project';
+const BASE_URL = 'http://7auth.local/';
+const BASE_PATH = __DIR__ . '/../';
